ed3bb23d2486db4e7626100e1afbdb643f729e52
Elias Ojala <91126885+ImRaven95@users.noreply.github.com>
2023-06-27 11:39:25 +0300

    404-fixed

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -21,7 +21,7 @@ export default function Skills () {
                     <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                         <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                             <div className='m-auto'>
-                                <Image src='/../static/next.png' 
+                                <Image src={nextimg}
                                 width='64' 
                                 height='64' 
                                 alt='/ '/>
@@ -34,7 +34,7 @@ export default function Skills () {
                     <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                         <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                             <div className='m-auto'>
-                                <Image src='/../static/tailwindcss.png' 
+                                <Image src={tailwindimg} 
                                 width='64' 
                                 height='64' 
                                 alt='/ '/>
@@ -60,7 +60,7 @@ export default function Skills () {
                     <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                         <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                             <div className='m-auto'>
-                                <Image src='/../static/js.png' 
+                                <Image src={jsimg} 
                                 width='64' 
                                 height='64' 
                                 alt='/ '/>
