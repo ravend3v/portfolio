50ac7b421c8bbebbe284ade97d0607f0233e72cc
Elias Ojala <91126885+ImRaven95@users.noreply.github.com>
2023-06-27 10:56:54 +0300

    error-fix

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import Link from "next/link"
 import ProjectItem from "./ProjectItems.jsx"
-import ProjectImg1 from "public/images/portfolio.png"
+import project1 from "public/images/portfolio.png"
 import Image from 'next/image.js';
 
 
@@ -14,7 +14,7 @@ export default function Projects () {
             <div className='grid md:grid-cols-2 gap-8'>
 
                 <div className='relative flex items-center justify-center h-auto w-full shadow-xl rounded-xl p-4 group hover:bg-gradient-to-r from-[#] to-[#]'>
-                    <Image className='rounded-xl group-hover:opacity-10' src={Project1} alt='/' />
+                    <Image className='rounded-xl group-hover:opacity-10' src={project1} alt='/' />
                 </div>
 
             </div>
